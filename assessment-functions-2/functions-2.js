03875339bbc56c9fc5f23af05d2699adfc0c356f
emkeator <emkeator@gmail.com>
2018-02-07 10:32:26 -0700

    broke out functions into 1, 2

--- assessment-functions-2/functions-2.js
+++ assessment-functions-2/functions-2.js
@@ -1,85 +1,6 @@
-//========= DO NOT TOUCH THIS CODE =========//
-var testing = {
-    a: hey ? true : false,
-    b: hi ? true : false,
-    c: hello ? true : false,
-}
-//============= CONTINUE BELOW =============//
 //+++++++++ Start Here! All problems are below. +++++++++//
 
 ///////////////////Problem 1//////////////////
-//Create a function called hey, using a function
-//declaration. hey should take in a 'name' parameter, and return
-//the string "Hey " plus the name.
-function hey(name){
-  return "Hey " + name
-}
-
-///////////////////Problem 2//////////////////
-//Create a function called hi, using a function
-//expression (without ES6). hi should take in a 'name' parameter, and return
-//the string "Hi " plus the name.
-var hi = function(name){
-  return "Hi " + name
-}
-
-///////////////////Problem 3//////////////////
-//Create a function called hello, using an arrow
-//function (use var). hello should take in a 'name' parameter, and return
-//the string "Hello " plus the name.
-var hello = (name) => "Hello " + name
-
-
-///////////////////Problem 4//////////////////
-//Create a function named fun, which takes in
-//a single string parameter, and returns that string
-//with "!!!!!" added to the end of it. Then, call fun 
-//with the string "Yay" and save the result of the function to a 
-//variable called funTimes.
-function fun(what){
-  return what + "!!!!!"
-}
-var funTimes = fun("Yay")
-
-
-///////////////////Problem 5//////////////////
-//Finish the function 'climb' below. Use the built-in local
-//variable arguments, within the function climb. 
-
-//If you do not know what the arguments variable is,
-//check out: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
-
-//Note: DO NOT alter the function climb to take in parameters.
-//The purpose of this exercise is to use the local arguments
-//variable within the function scope.
-
-//If there is a string at arguments[0], and false
-//at arguments[1], return "On belay?". 
-
-//If there is a string at arguments[0], and true at arguments[1],
-//return "Climbing!"
-
-//Otherwise, return "Let's set up the belay rope before we climb."
-
-function climb(){
-  //What is the arguments variable? Try calling climb
-  //and passing in ("Ellie", true) and see what is logged
-  //to the console.
-  console.log('function climb\'s local arguments var - ', arguments)
-
-  //CODE HERE - DO NOT TOUCH THE CODE ABOVE!
-  if(arguments[0] && typeof arguments[0] === 'string'){
-    if(arguments[1]){
-      return "Climbing!"
-    } else {
-      return "On belay?"
-    }
-  } else {
-    return "Let's set up the belay rope before we climb."
-  }
-}
-
-///////////////////Problem 6//////////////////
 //Create a function called keyword. Inside, 
 //save a function expression to a variable
 //called key. That function expression should
@@ -94,7 +15,7 @@ function keyword(){
   return key
 }
 
-///////////////////Problem 7//////////////////
+///////////////////Problem 2//////////////////
 //Answer the questions based on the following code:
 //NOTE: Un-comment the line that best fits.
 
@@ -134,7 +55,7 @@ var seven = "argument"
 // var seven = "variable"
 
 
-///////////////////Problem 8//////////////////
+///////////////////Problem 3//////////////////
 //Answer the questions based on the following code:
 //NOTE: Un-comment the line that best fits.
 
@@ -176,7 +97,7 @@ var answer3 = "Leaving early...at index 0"
 var answer4 = "No, by that line of code i is array.length."
 // var answer4 = "No, by that line of code i is 1 less than array.length."
 
-///////////////////Problem 9//////////////////
+///////////////////Problem 4//////////////////
 //Answer the question based on the following code:
 
 function funcA(){
@@ -195,7 +116,7 @@ var funcC = () => 3
 var answer5 = ["funcA"]
 
 
-///////////////////Problem 10//////////////////
+///////////////////Problem 5//////////////////
 //Create a function called janus that takes in a single
 //parameter, a boolean. Inside the function janus,
 //create a function called left, which returns the string
